c5fb0b684e45266aea1db5fd07645f7da67d5fd1
Erzbir <100007608+Erzbir@users.noreply.github.com>
2022-03-22 21:26:29 +0800

    Delete No.2 directory

--- No.2/ex1.py
+++
@@ -1,8 +0,0 @@
-while True:
-    a=int(input("输入一个整数:"))
-    b=int(input("输入一个整数:"))
-    print(a,b)
-    a=a^b
-    b=a^b
-    a=a^b
-    print(a,b)
--- No.2/ex7.py
+++
@@ -1,4 +0,0 @@
-from cmath import pi
-r=int(input("输入半径:"))
-print("面积=%f"%(pi*r*r))
-print("周长=%f"%(pi*2*r))
